0f13e936e78c661a0f773103fc40ffb8060fdbc0
Paulo Sérgio <paulosergioferreira.ti@gmail.com>
2018-06-25 16:04:50 -0300

    ajuste no layout da tela login

--- layout/login.php
+++ layout/login.php
@@ -7,21 +7,23 @@
 <body>
 
 <div style="position: absolute; width: 100px; height: 100px; top: 20%; left: 39%;">
-<form>
+<div class="form-row">
+<form method="post" action="cadastro.php">
   <div class="form-group" >
     <label for="exampleInputEmail1">Login</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entre com seu login" style="width: 300px">
   </div>
-  
+   
   <div class="form-group">
     <label for="exampleInputPassword1">Senha</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Entre com sua senha" style="width: 300px">
   </div>
 
-  <input type="submit" name="acessar" class="btn btn-primary" value="Entrar">
+  <input type="submit" name="acessar" class="btn btn-primary" value="Entrar" >
   <a href="cadastro.php" class="btn btn-danger">Cadastra-se</a>
  
 </form>
+</div>
 </div>
 </body>
 </html>
